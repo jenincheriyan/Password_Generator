23cb220369e044133bffaa77a1bbc7ee558ad221
jenin <jenincheriyan@gmail.com>
2025-07-20 13:25:02 +0530

    Search Feature Added

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import tkinter as tk
 from json import JSONDecodeError
-from tkinter import Entry, Button,messagebox
+from tkinter import Entry, Button,messagebox,font
 import secrets,string
-LENGTH =12
 import json
+LENGTH =8
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Search
+def search():
+    website= website_entry.get()
+    with open("password.json",'r') as data_file:
+        try:
+            data_in=json.load(data_file)
+            password= data_in.get(website,f'No {website} found').get('password',"not found")
+        except AttributeError:
+            messagebox.showinfo("Error", message="Enter The Website Name")
+        except JSONDecodeError:
+            messagebox.showinfo(title="Error",message='What bro! Nothing here')
+        else:
+            messagebox.showinfo(title="Information", message=f'Your Password is: {password}')
+
+
+#Passwordgenerator
 def generate_password():
     character = string.ascii_letters+string.digits + string.punctuation
     password= ''.join(secrets.choice(character) for _ in range(LENGTH))
@@ -13,7 +28,7 @@ def generate_password():
     password_entry.insert(0, password)
 
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+#SavePassword
 def save_password():
         website=website_entry.get()
         email=email_entry.get()
@@ -38,10 +53,11 @@ def save_password():
                 data.update(new_data)
                 with open("password.json",'w') as data_file:
                     json.dump(data,data_file,indent=4)
-            password_entry.delete(0, tk.END)
-            website_entry.delete(0, tk.END)
-# ---------------------------- UI SETUP ------------------------------- #
 
+            finally:
+                password_entry.delete(0, tk.END)
+                website_entry.delete(0, tk.END)
+#UI
 window= tk.Tk()
 window.title("Password Generator")
 window.config(padx=40,pady=40, bg="white")
@@ -59,20 +75,22 @@ label_password= tk.Label(window, text="Password:",bg="white",highlightthickness=
 label_password.grid(row=3,column=0)
 
 #entries
-website_entry = Entry(width=35,)
-website_entry.grid(row=1,column=1,columnspan=2)
+website_entry = Entry(width=24)
+website_entry.grid(row=1,column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2,column=1,columnspan=2)
-email_entry.insert(0,"email@gmail.com")
-password_entry = Entry(width=21)
+email_entry.insert(0,"Email@gmail.com")
+password_entry = Entry(width=24)
 password_entry.grid(row=3,column=1)
 
 #buttons
-generate_button= Button(text="Generator",width=10,command=generate_password,bg="white",highlightthickness=0)
-generate_button.grid(row=3,column=2)
-add_button= Button(text="add",width=35,command=save_password,bg="white",highlightthickness=0)
+generate_button= Button(text="Generator",width=7,command=generate_password,bg="white",highlightthickness=0)
+generate_button.grid(row=3,column=2, pady=0,padx=0)
+add_button= Button(text="add",width=32,command=save_password,bg="white",highlightthickness=0)
 add_button.grid(row=4,column=1,columnspan=2)
+search_button= Button(text="Search",width=7,command=search,bg="white",highlightthickness=0)
+search_button.grid(row=1,column=2,padx=0,pady=0)
 
 
 window.mainloop()
